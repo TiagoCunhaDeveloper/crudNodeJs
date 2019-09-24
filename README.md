455fe9b8f6a4d34d5fc68b796983c0018de7996d
tiago <tiagocunha419@gmail.com>
2019-09-24 17:11:58 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CRUD em NodeJS e mongoDB
+
+### Projeto realizado no curso "Curso NodeJS" da rocketseat.
---
+++ src/models/Product.js
@@ -0,0 +1,25 @@
+const moongose = require ('mongoose');
+const moongosePaginate = require('mongoose-paginate');
+
+const ProductSchema = new moongose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    url: {
+        type: String,
+        required: true,
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now,
+    }
+});
+
+ProductSchema.plugin(moongosePaginate);
+
+moongose.model('Product', ProductSchema);
